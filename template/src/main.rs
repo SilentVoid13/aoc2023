5fde7233cca6ae14d2d4833ac59437b1c70d4530
SilentVoid13 <51264226+SilentVoid13@users.noreply.github.com>
2023-12-01 16:49:49 +0100

    feat: initial commit

---
+++ template/src/main.rs
@@ -0,0 +1,30 @@
+use std::time::Instant;
+
+use color_eyre::eyre::{eyre, Result};
+
+fn part1(input: &str) -> Result<usize> {
+    Err(eyre!("todo"))
+}
+
+fn part2(input: &str) -> Result<usize> {
+    Err(eyre!("todo"))
+}
+
+fn main() -> Result<()> {
+    color_eyre::install()?;
+
+    let input = include_str!("../input");
+
+    let instant = Instant::now();
+    let res = part1(input)?;
+    let time = Instant::now() - instant;
+    println!("[*] part 1: {} ({:?})", res, time);
+
+    let instant = Instant::now();
+    let res = part2(input)?;
+    let time = Instant::now() - instant;
+    println!("[*] part 2: {} ({:?})", res, time);
+
+    Ok(())
+}
+
